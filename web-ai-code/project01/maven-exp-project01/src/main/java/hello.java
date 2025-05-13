306a7e1bf69d1643561586804a34735e6397ffa3
HHZZY666 <hhzzy666@gmail.com>
2025-05-13 21:20:28 +0800

    Add files via upload

--- web-ai-code/project01/maven-exp-project01/src/main/java/hello.java
+++ web-ai-code/project01/maven-exp-project01/src/main/java/hello.java
@@ -2,4 +2,11 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Maven");
     }
+
+    /*@Test
+    public static void test() {
+        UserService userService = new UserService();
+        System.out.println(userService.getAge("110101199001011234"));
+        System.out.println(userService.getGender("110101199001011234"));
+    }*/
 }
