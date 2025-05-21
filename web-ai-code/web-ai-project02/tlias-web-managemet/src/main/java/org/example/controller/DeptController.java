98ac60855e33cbc21bb3aae6575baac077163744
HHZZY666 <hhzzy666@gmail.com>
2025-05-21 21:52:36 +0800

    Add files via upload

---
+++ web-ai-code/web-ai-project02/tlias-web-managemet/src/main/java/org/example/controller/DeptController.java
@@ -0,0 +1,27 @@
+package org.example.controller;
+
+import org.example.pojo.Dept;
+import org.example.pojo.Result;
+import org.example.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    //@RequestMapping(value = "/depts",method = RequestMethod.GET) //method:指定请求方式
+    @GetMapping("/depts")
+    public Result list() {
+        System.out.println("查询全部部门数据");
+        List<Dept> deptList = deptService.findAll();
+        return Result.success(deptList);
+    }
+}
---
+++ web-ai-code/web-ai-project02/tlias-web-managemet/src/main/java/org/example/mapper/DeptMapper.java
@@ -0,0 +1,27 @@
+package org.example.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.example.pojo.Dept;
+
+import java.util.List;
+
+@Mapper
+public interface DeptMapper {
+    /**
+     * 查询所有部门数据
+     */
+    //方式一：手动结构映射
+    /*@Results({
+            @Result(column = "create_time", property = "createTime"),
+            @Result(column = "update_time", property = "updateTime")
+    })*/
+
+    //方式二：起别名
+    //@Select("select id, name, create_time createTime, update_time updateTime from dept order by update_time desc")
+
+    @Select("select id, name, create_time, update_time from dept order by update_time desc")
+    List<Dept> findAll();
+}
