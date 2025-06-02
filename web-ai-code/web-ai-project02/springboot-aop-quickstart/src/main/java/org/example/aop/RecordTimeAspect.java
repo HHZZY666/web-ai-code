a95f9de60bf9503656c4f525776009d9f03b03aa
HHZZY666 <hhzzy666@gmail.com>
2025-06-02 12:49:03 +0800

    Add files via upload

---
+++ web-ai-code/web-ai-project02/springboot-aop-quickstart/src/main/java/org/example/aop/RecordTimeAspect.java
@@ -0,0 +1,28 @@
+package org.example.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Aspect
+public class RecordTimeAspect {
+
+    @Around( "execution(* org.example.service.impl.*.*(..))")
+    public Object recordTime(ProceedingJoinPoint pjp) throws Throwable {
+        //1.记录开始时间
+        long begin = System.currentTimeMillis();
+
+        //2.执行目标方法
+        Object result = pjp.proceed();
+
+        //3.记录结束时间
+        long end = System.currentTimeMillis();
+        log.info("方法 {} 耗时：{}ms",pjp.getSignature(), end-begin);
+        return result;
+
+    }
+}
